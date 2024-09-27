b0342066ced609d11df38d099ddeac17710de6bd
boatzaza1212 <s6501075510303@email.kmutnb.ac.th>
2024-09-27 15:56:22 +0700

    b

--- post.js
+++ post.js
@@ -1,31 +1,31 @@
 async function submitData() {
-    // รับข้อมูลจากฟอร์ม
-    const studentId = document.getElementById("studentId").value;
-    const studentName = document.getElementById("studentName").value;
+  // รับข้อมูลจากฟอร์ม
+  const studentName = document.getElementById("studentName").value;
+  const studentID = document.getElementById("studentID").value; 
 
-    // สร้างข้อมูลในรูปแบบ JSON
-    const data = {
-        id: studentId,
-        name: studentName
-    };
+  // สร้างข้อมูลในรูปแบบ JSON
+  const data = {
+      id: studentID,
+      name: studentName
+  };
 
-    // ส่งข้อมูลไปยัง API โดยใช้ fetch
-    try {
-        const response = await fetch("https://206.189.146.138/api/students?fbclid=IwY2xjawFjSItleHRuA2FlbQIxMAABHZaRS60V-dJg96FfbYfCYJ2HhYkTb9bf17ofb_3xGApZoy9fIVcILJx1_w_aem_UTX6ZfNqqiDJ1eMty-u7Hw", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(data)
-        });
+  // ส่งข้อมูลไปยัง API โดยใช้ fetch
+  try {
+      const response = await fetch("https://206.189.146.138/api/students", {
+          method: "POST",
+          headers: {
+              "Content-Type": "application/json"
+          },
+          body: JSON.stringify(data)
+      });
 
-        if (response.ok) {
-            alert("ได้รับแล้วจั๊ฟผม");
-        } else {
-            alert("ยังไม่เข้าจั๊ฟพรี่!!!");
-        }
-    } catch (error) {
-        console.error("Error:", error);
-        alert("ยังไม่เข้าจั๊ฟพรี่!!!");
-    }
+      if (response.ok) {
+          alert("Successfully");
+      } else {
+          alert("!ERROR!");
+      }
+  } catch (error) {
+      console.error("Error:", error);
+      alert("!ERROR!");
+  }
 }
